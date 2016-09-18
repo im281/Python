498328203d8a99b1cf5e670edfbb3040ae7f56dd
im281 <iman.mohtashemi@gmail.com>
2016-09-18 08:57:39 -0700

    data analysis book data

--- Python Scripts/ChemSpiderTest.py
+++ Python Scripts/ChemSpiderTest.py
@@ -5,14 +5,13 @@ import TextFileUtilities
 
 #c = ChemSpiderQuery.find('Dodecanoic Acid')
 c = ChemSpiderQuery.find_one('Dodecanoic Acid')
-print c
+print (c)
 c = ChemSpiderQuery.Compound(3756)
-print c.commonname
-print c.monoisotopicmass
-print c.commonname
+print (c.commonname)
+print (c.monoisotopicmass)
+print (c.commonname)
 
 t = TextFileUtilities.TextUtilities()
 #file = "C:\Users\Iman\Desktop\Nist.txt"
 #t.ChemSpiderSearchFile(file)
 t.WriteTextFile('testing.txt')
-
